4fad32a27e469a401ccca2f1294bfa02ce4bcbbb
Ross Perkel <rossperkel@gmail.com>
2019-11-27 18:50:52 -0600

    basic js set up

---
+++ index.js
@@ -0,0 +1,7 @@
+
+// base set up
+const canvas = document.getElementById('canvasOne');
+const ctx = canvas.getContext('2d');
+canvas.height = window.innerHeight;
+canvas.width = window.innerWidth;
+let particlesArray = []
